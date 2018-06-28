c0b94dc6f3e18b1fe540a68af7d0e799d7ec54a9
liuxiaobing <ch_lxb@163.com>
2018-06-28 18:41:47 +0800

    update

---
+++ LogLib/LogLib.h
@@ -0,0 +1,19 @@
+//
+//  LogLib.h
+//  LogLib
+//
+//  Created by lxb on 2018/6/28.
+//  Copyright © 2018年 lxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LogLib.
+FOUNDATION_EXPORT double LogLibVersionNumber;
+
+//! Project version string for LogLib.
+FOUNDATION_EXPORT const unsigned char LogLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogLib/PublicHeader.h>
+
+
