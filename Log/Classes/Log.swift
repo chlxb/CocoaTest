81743067a7bd9edc2aee6e9f6006d793561b1cca
liuxiaobing <ch_lxb@163.com>
2018-06-29 10:08:26 +0800

    update

--- Log/Classes/Log.swift
+++ Log/Classes/Log.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 public enum LogLevel: String {
     case none = "none", warning = "warning", error = "error", verbose = "verbose"
@@ -14,10 +15,16 @@ public enum LogLevel: String {
 
 public class Log {
     public static var level: LogLevel = .none
-    public static func log(_ message: String, level: LogLevel) {
+    public static func log(_ message: Any, level: LogLevel) {
         guard level != .none else { return }
         if level == self.level {
             print("\(level.rawValue): \(message)")
+        }
+    }
+    
+    public static func request() {
+        Alamofire.request("http://www.baidu.com", method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseJSON { (request) in
+            log(request, level: .warning)
         }
     }
 
